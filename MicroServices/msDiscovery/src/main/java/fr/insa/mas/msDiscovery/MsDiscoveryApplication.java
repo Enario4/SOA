0020cc0c8f268b10464a37eadd7095f506b77582
Oriane <orianeberry4@gmail.com>
2022-11-29 15:15:01 +0100

    page 7 td cloud 3

---
+++ MicroServices/msDiscovery/src/main/java/fr/insa/mas/msDiscovery/MsDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package fr.insa.mas.msDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiscoveryApplication.class, args);				
+	}
+
+}
